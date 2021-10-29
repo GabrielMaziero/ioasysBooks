9affe83ac6295938c8b1efbec14324f4afd5c27b
gcmaziero <gcmaziero@rd.com.br>
2021-10-29 19:27:39 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Login from './src/views/login/login';
+
+const App = () => {
+  return <Login />;
+};
+
+export default App;
---
+++ src/views/login/login.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+const Login = () => {
+  return <SafeAreaView />;
+};
+
+export default Login;
