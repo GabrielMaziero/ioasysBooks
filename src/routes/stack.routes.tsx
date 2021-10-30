69e3fb55354b88d10f3fd5f456929239681963d3
gcmaziero <gcmaziero@rd.com.br>
2021-10-30 11:23:21 -0300

    feat: created navigation and routes

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Login} from '../views/index';
+
+const Stack = createNativeStackNavigator();
+
+export const StackViews = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Login" component={Login} />
+    </Stack.Navigator>
+  );
+};
