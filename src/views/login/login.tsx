c25666bc4239dd255ce74420f0ddf017c35d46e4
gcmaziero <gcmaziero@rd.com.br>
2021-10-30 17:29:17 -0300

    refactor: add axios lib and refactor login

--- src/views/login/login.tsx
+++ src/views/login/login.tsx
@@ -2,6 +2,7 @@ import {useNavigation} from '@react-navigation/core';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import React from 'react';
 import {RootStackParamList} from '../../routes/stack.routes';
+import api from '../../services/api';
 import {ValidateEmail} from '../../utils/validate-email';
 import {
   LoginButton,
@@ -30,20 +31,10 @@ const Login: React.FC = () => {
         console.error('Email invalido');
         return;
       }
-      const response = await fetch(
-        'https://books.ioasys.com.br/api/v1/auth/sign-in',
-        {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            email: email,
-            password: password,
-          }),
-        },
-      );
+      const response = await api.post('/auth/sign-in', {
+        email: email,
+        password: password,
+      });
       navigate('Home');
       console.log('AQUI====>', response);
     } catch (error) {
